04484bc7413add4d7774fb225f72464ffd7bc6c1
Abby Thompson <abby.thompson@myob.com>
2019-09-03 13:38:54 +1200

    level creator refactored out repeat code

--- Pacman/PacmanLibrary/BoardLogic/LevelCreator.cs
+++ Pacman/PacmanLibrary/BoardLogic/LevelCreator.cs
@@ -7,100 +7,76 @@ namespace PacmanLibrary.BoardLogic
     {
         public int PacmanRow { get; private set; }
         public int PacmanCol { get; private set; }
+
         public ICell[,] CreateLevel(int level)
         {
             switch (level)
-            {
-                case 0:
-                    break;
+            { 
                 case 1:
                     return CreateLevelOne();
                 case 2:
                     return CreateLevelTwo();
                 default:
                     return CreateLevelOne();
-            }
+            } 
+        }
+        private ICell[,] CreateLevelOne()
+        {
+            var level = new[,]
+            {
+                {1, 0, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 1}, 
+                {1, 0, 2, 0, 0, 1}, 
+                {1, 0, 1, 1, 0, 1},  
+            };
 
-            return CreateLevelOne();
+            return GenerateCellsArrayForLevel(level);
         }
 
         private ICell[,] CreateLevelTwo()
         {
-            var rows = 5;
-            var cols = 7;
+            var level = new[,]
+            {
+                {1, 1, 0, 1, 1, 1},
+                {1, 0, 0, 0, 0, 1}, 
+                {1, 1, 2, 1, 0, 1}, 
+                {1, 0, 0, 1, 0, 1}, 
+                {1, 0, 1, 1, 0, 1}, 
+                {1, 0, 0, 0, 0, 1}, 
+                {1, 1, 0, 1, 1, 1}, 
+            };
+
+            return GenerateCellsArrayForLevel(level);
+        }
+
+        private ICell[,] GenerateCellsArrayForLevel(int[,] level)
+        {
+            var rows = level.GetLength(0);
+            var cols = level.GetLength(1);
             var cells = new ICell[rows, cols];
 
             for (var i = 0; i < rows; i++)
             {
                 for (var j = 0; j < cols; j++)
                 {
-                    switch (i)
+                    switch (level[i, j])
                     {
                         case 0:
-                        case 4:
-                            cells[i, j] = new Cell(State.Wall);
+                            cells[i, j] = new Cell(State.Food);
                             break;
                         case 1:
-                        case 3:
-                        {
-                            if (j == 0 || j == 6)
-                            {
-                                cells[i, j] = new Cell(State.Wall);
-                            }
-                            else
-                            {
-                                cells[i, j] = new Cell(State.Food);
-                            }
-
-                            break;
-                        }
-
-                        case 2 when (j > 1 && j < 5):
                             cells[i, j] = new Cell(State.Wall);
                             break;
-                        default:
-                            cells[i, j] = new Cell(State.Food);
+                        case 2:
+                            cells[i, j] = new Cell(State.Pacman);
+                            PacmanCol = i;
+                            PacmanRow = j;
                             break;
                     }
                 }
             }
-            PacmanCol = 1;
-            PacmanRow = 1;
-            cells[PacmanRow,PacmanCol].SetState(State.Pacman);
-
 
             return cells;
-        }
-
-        private ICell[,] CreateLevelOne()
-        {
-            var rows = 5;
-            var cols = 5;
-            var cells = new ICell[rows, cols];
-
-            for (var i = 0; i < rows; i++)
-            {
-                for (var j = 0; j < cols; j++)
-                {
-                    if (i == 0 || i == rows - 1)
-                    {
-                        cells[i, j] = new Cell(State.Wall);
-                    }
-                    else if (j == 0 || j == cols - 1)
-                    {
-                        cells[i, j] = new Cell(State.Wall);
-                    }
-                    else
-                    {
-                        cells[i, j] = new Cell(State.Food);
-                    }
-                }
-            } 
-            PacmanCol = 1;
-            PacmanRow = 1;
-            cells[PacmanRow,PacmanCol].SetState(State.Pacman);
-
-            return cells;
-        }
+        } 
     }
 }
